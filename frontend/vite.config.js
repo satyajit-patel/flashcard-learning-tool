86a26e33e7c7c1585ffdbee66c0e6ef414705300
satyajit-patel <satyajitpatel770001@gmail.com>
2025-03-10 20:53:58 +0530

    bug fix

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    sourcemap: true, // Set to 'inline' for development or true for production
+  },
 })
